3a0f28d24b1ad0698e46254ec5bc09ac72ff7923
awdem <wdemontmollin@gmail.com>
2023-06-21 14:33:38 +0100

    tests: adds tests for updating quality of existing items

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,11 +1,148 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
-describe("Gilded Rose", () => {
-  test("when getItems is called, it returns the items array", () => {
+describe('Gilded Rose', () => {
+  test('when getItems is called, it returns the items array', () => {
     const items = [new Item("item", 0, 0)]
     const gildedRose = new Shop(items);
 
-    shopItems =gildedRose.getItems()
+    shopItems = gildedRose.getItems()
     expect(shopItems[0].name).toBe("item");
   });
+
+  describe('#updateQuality', () => {
+    describe('given an in-date item', () => {
+      test('it lowers the quality of a normal item by 1', () => {
+        const items = [new Item("item", 10, 20)]
+        const gildedRose = new Shop(items);
+
+        gildedRose.updateQuality()
+        const item = gildedRose.getItems()[0]
+        expect(item.quality).toBe(19);    
+      });
+      
+      test('it does not change the quality of Sulfuras', () => {
+        const items = [new Item("Sulfuras, Hand of Ragnaros", 10, 80)]
+        const gildedRose = new Shop(items);
+
+        gildedRose.updateQuality()
+        const item = gildedRose.getItems()[0]
+        expect(item.quality).toBe(80);    
+      });
+
+      test('it raises the quality of Aged Brie by 1', () => {
+        const items = [new Item("Aged Brie", 10, 20)]
+        const gildedRose = new Shop(items);
+
+        gildedRose.updateQuality()
+        const agedBrie = gildedRose.getItems()[0]
+        expect(agedBrie.quality).toBe(21);    
+      });
+      
+      test('when SellIn > 10, it raises the quality of Backstage passes by 1 ', () => {
+        const items = [new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20)]
+        const gildedRose = new Shop(items);
+
+        gildedRose.updateQuality()
+        const backstagePass = gildedRose.getItems()[0]
+        expect(backstagePass.quality).toBe(21);    
+      });
+
+      test.each([10, 9, 8, 7, 6])(
+        'when SellIn is 6..10, it raises the quality of Backstage passes by 2',
+        (sellIn) => {
+          const items = [new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, 0)]
+          const gildedRose = new Shop(items);
+
+          gildedRose.updateQuality()
+          const backstagePass = gildedRose.getItems()[0]
+          expect(backstagePass.quality).toBe(2);    
+        }
+      );
+
+      test.each([5, 4, 3, 2, 1])(
+        'when SellIn is 1..5, it raises the quality of Backstage passes by 3',
+        (sellIn) => {
+          const items = [new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, 0)]
+          const gildedRose = new Shop(items);
+
+          gildedRose.updateQuality()
+          const backstagePass = gildedRose.getItems()[0]
+          expect(backstagePass.quality).toBe(3);    
+        }
+      );
+
+      test('Backstage pass quality cannot go above 50', () => {
+        const items = [
+          new Item("Backstage passes to a TAFKAL80ETC concert", 15, 50),
+          new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50),
+          new Item("Backstage passes to a TAFKAL80ETC concert", 5, 50),
+        ]
+        const gildedRose = new Shop(items);
+
+        gildedRose.updateQuality()
+        const updatedItems = gildedRose.getItems()
+        const totalQuality = updatedItems.reduce((sum, item) => sum + item.quality, 0)
+        expect(totalQuality).toBe(10); 
+      });
+    })
+
+    describe('given an out-of-date item', () => {
+      test('it lowers the quality of a normal item by 2, rather than 1', () => {
+        const items = [new Item("item", 0, 20)]
+        const gildedRose = new Shop(items);
+
+        gildedRose.updateQuality()
+        const item = gildedRose.getItems()[0]
+        expect(item.quality).toBe(18);      
+      })
+
+      test('it does not change the quality of Sulfuras', () => {
+        const items = [new Item("Sulfuras, Hand of Ragnaros", 0, 80)]
+        const gildedRose = new Shop(items);
+
+        gildedRose.updateQuality()
+        const item = gildedRose.getItems()[0]
+        expect(item.quality).toBe(80);    
+      });
+      test('it raises the quality of Aged Brie by 1', () => {
+        const items = [new Item("Aged Brie", 0, 20)]
+        const gildedRose = new Shop(items);
+
+        gildedRose.updateQuality()
+        const agedBrie = gildedRose.getItems()[0]
+        expect(agedBrie.quality).toBe(21);    
+      });
+
+      test('Aged Brie quality cannot go above 50', () => {
+        const items = [new Item("Aged Brie", 0, 50)]
+        const gildedRose = new Shop(items);
+
+        gildedRose.updateQuality()
+        const agedBrie = gildedRose.getItems()[0]
+        expect(agedBrie.quality).toBe(50);   
+      });
+
+      test('it sets the quality of Backstage passes to 0', () => {
+        const items = [new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+        const gildedRose = new Shop(items);
+
+        gildedRose.updateQuality()
+        const backstagePass = gildedRose.getItems()[0]
+        expect(backstagePass.quality).toBe(0);    
+      });
+
+      test('of the items that lose quality, no item quality can go below 0', () => {
+        const items = [
+          new Item("regular item", 0, 0),
+          new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0),
+        ]
+        const gildedRose = new Shop(items);
+
+        gildedRose.updateQuality()
+        const updatedItems = gildedRose.getItems()
+        const totalQuality = updatedItems.reduce((sum, item) => sum + item.quality, 0)
+        expect(totalQuality).toBe(0);
+      });
+    })
+  })
 });
