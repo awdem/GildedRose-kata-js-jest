b55d001fb9a64b39e49a4d189172e8720db62ef5
awdem <wdemontmollin@gmail.com>
2023-06-21 17:44:56 +0100

    tests: adjusts the texttest file to have more examples, show more days

--- test/texttest_fixture.js
+++ test/texttest_fixture.js
@@ -3,19 +3,18 @@ const { Shop, Item } = require("../src/gilded_rose");
 
 const items = [
   new Item("+5 Dexterity Vest", 10, 20),
-  new Item("Aged Brie", 2, 0),
+  new Item("Aged Brie", 1, 0),
   new Item("Elixir of the Mongoose", 5, 7),
   new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+  new Item("Sulfuras, The Other Hand of Ragnaros", -1, 80),
   new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-
-  // This Conjured item does not work properly yet
+  new Item("Backstage passes to a Leeroy Jenkins concert", 10, 49),
+  new Item("Backstage passes to WoW", 1, 10),
   new Item("Conjured Mana Cake", 3, 6),
+  new Item("Conjured Pants", 1, 10),
 ];
 
-const days = Number(process.argv[2]) || 2;
+const days = Number(process.argv[2]) || 3;
 const gildedRose = new Shop(items);
 
 console.log("OMGHAI!");
